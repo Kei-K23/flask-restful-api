080394d529bb789d943b87a99bb0b7797ecce68d
Kei-K23 <keiksl2301@gmail.com>
2024-10-21 18:29:14 +0630

    Add item models

---
+++ models/item.py
@@ -0,0 +1,45 @@
+from db import create_connection
+
+class ItemModel:
+    def __init__(self, _id, name ,price, owner_id):
+        self.id = _id
+        self.name = name
+        self.price = price
+        self.owner_id = owner_id
+    
+    @classmethod
+    def find_by_username(cls, name):
+        conn = create_connection()
+        cursor = conn.cursor()
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        if row:
+            return cls(*row)
+    
+    @classmethod
+    def find_by_id(cls, id):
+        conn = create_connection()
+        cursor = conn.cursor()
+        query = "SELECT * FROM items WHERE id=?"
+        result = cursor.execute(query, (id,))
+        row = result.fetchone()
+        if row:
+            return cls(*row)
+
+    @classmethod
+    def find_all(cls):
+        conn = create_connection()
+        cursor = conn.cursor()
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        rows = result.fetchall()
+        return [cls(*row) for row in rows]
+
+    def save_to_db(self):
+        conn = create_connection()
+        cursor = conn.cursor()
+        query = "INSERT INTO items (name, price, owner_id) VALUES (?, ?, ?)"
+        cursor.execute(query, (self.name, self.price, self.owner_id,))
+        conn.commit()
+        conn.close()
